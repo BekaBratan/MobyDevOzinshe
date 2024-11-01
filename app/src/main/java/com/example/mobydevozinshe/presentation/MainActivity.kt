f05813716debad149c8d2e4d7b8bcfb843e44b92
Baibolat Bekarys <210103206@stu.sdu.edu.kz>
2024-11-01 20:40:32 +0500

    Fix home page navigation bar height, fix full screen and made always portrait orientation

--- app/src/main/java/com/example/mobydevozinshe/presentation/MainActivity.kt
+++ app/src/main/java/com/example/mobydevozinshe/presentation/MainActivity.kt
@@ -35,11 +35,18 @@ class MainActivity : AppCompatActivity(), NavigationHostProvider {
 
     @Suppress("DEPRECATION")
     private fun hideSystemUI() {
-        window.decorView.systemUiVisibility = (
-                View.SYSTEM_UI_FLAG_FULLSCREEN
-                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                )
+        val flags =
+            View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_IMMERSIVE or
+                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        window.decorView.systemUiVisibility = flags
+        supportActionBar?.hide()
     }
 
     override fun setNavigationVisibility(visible: Boolean) {
